7134987503bd032c809c9810cf5f0a8619f161e5
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-15 10:35:37 +0100

    exo3

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $km = "1";
+    echo "$km";
+    ?>
+    <?php
+    $km = "3";
+    echo "$km";
+    ?>
+    <?php
+    $km = "125";
+    echo "$km";
+    ?>
+
+  </body>
